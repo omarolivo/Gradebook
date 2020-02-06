bd4c7d24ce3414dd7fabcedc5be6d2f44191ad84
Omar Olivo <omar.olivo@live.com>
2020-02-06 16:53:17 -0500

    add gradebook module

---
+++ src/app/gradebook/gradebook.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GradebookRoutingModule } from './gradebook-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GradebookRoutingModule
+  ]
+})
+export class GradebookModule { }
