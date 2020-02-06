2d0057aba6e529506f268290b966638a4cf1b126
OmarOlivo <pisqko@gmail.com>
2020-02-06 15:13:27 -0500

    Delete component. Recreate as a Feature Module

--- src/app/gradebook/gradebook.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'gb-gradebook',
-  templateUrl: './gradebook.component.html',
-  styleUrls: ['./gradebook.component.css']
-})
-export class GradebookComponent { }
