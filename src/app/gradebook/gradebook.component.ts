6740078488479d23f889e9c2fc36be0a1691cf43
Linus Liang <LinusLiang@protonmail.com>
2020-02-06 12:56:19 -0500

    Add Gradebook component

---
+++ src/app/gradebook/gradebook.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gb-gradebook',
+  templateUrl: './gradebook.component.html',
+  styleUrls: ['./gradebook.component.css']
+})
+export class GradebookComponent { }
