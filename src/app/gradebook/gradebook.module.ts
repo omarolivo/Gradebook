734f4dc166e44c3b5ac702ee7b4875c777def23c
Linus Liang <LinusLiang@protonmail.com>
2020-02-07 12:16:53 -0500

    Add Students component
    Issue #11

--- src/app/gradebook/gradebook.module.ts
+++ src/app/gradebook/gradebook.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { GradebookRoutingModule } from './gradebook-routing.module';
 import { GradebookComponent } from './gradebook.component';
 import { StudentComponent } from './student/student.component';
+import { StudentsComponent } from './students/students.component';
 
 
 @NgModule({
   declarations: [
     GradebookComponent,
     StudentComponent,
+    StudentsComponent
   ],
   imports: [
     CommonModule,
