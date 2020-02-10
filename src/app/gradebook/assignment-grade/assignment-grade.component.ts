f950d52a0a9911dc2293eaca356c3932df963cbc
Linus Liang <LinusLiang@protonmail.com>
2020-02-10 07:56:39 -0500

    Add AssignmentGrade and AssignmentGrades Component

---
+++ src/app/gradebook/assignment-grade/assignment-grade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gb-assignment-grade',
+  templateUrl: './assignment-grade.component.html',
+  styleUrls: ['./assignment-grade.component.css']
+})
+export class AssignmentGradeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
