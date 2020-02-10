d14b3efc5dc65c5de569c3a2a31475235bdb3911
Linus Liang <LinusLiang@protonmail.com>
2020-02-09 23:03:21 -0500

    Fix data model import path

--- src/app/gradebook/assignments/assignments.component.ts
+++ src/app/gradebook/assignments/assignments.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Assignment } from '../gradebook/models/gradebook.models';
+import { Assignment } from '../models/gradebook.models';
 
 @Component({
   selector: 'gb-assignments',
