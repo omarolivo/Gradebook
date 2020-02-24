22cd4539b87a562da19b67831bf6ef89ba11ee5e
Linus Liang <LinusLiang@protonmail.com>
2020-02-24 12:53:01 -0500

    Remove unused Students and Assignments components

--- src/app/gradebook/gradebook.module.ts
+++ src/app/gradebook/gradebook.module.ts
@@ -4,9 +4,7 @@ import { CommonModule } from '@angular/common';
 import { GradebookRoutingModule } from './gradebook-routing.module';
 import { GradebookComponent } from './gradebook.component';
 import { StudentComponent } from './student/student.component';
-import { StudentsComponent } from './students/students.component';
 import { AssignmentComponent } from './assignment/assignment.component';
-import { AssignmentsComponent } from './assignments/assignments.component';
 import { AssignmentGradeComponent } from './assignment-grade/assignment-grade.component';
 import { AssignmentGradesComponent } from './assignment-grades/assignment-grades.component';
 import { AssignmentsListComponent } from './assignment-list/assignment-list.component';
@@ -17,9 +15,7 @@ import { StudentsListComponent } from './student-list/student-list.component';
   declarations: [
     GradebookComponent,
     StudentComponent,
-    StudentsComponent,
     AssignmentComponent,
-    AssignmentsComponent,
     AssignmentGradeComponent,
     AssignmentGradesComponent,
 
