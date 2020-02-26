b7a892d6de4e0d008b8878df969b9243c2d0328e
Linus Liang <LinusLiang@protonmail.com>
2020-02-26 12:19:06 -0500

    Remove unused StudentComponent

--- src/app/gradebook/gradebook.module.ts
+++ src/app/gradebook/gradebook.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { GradebookRoutingModule } from './gradebook-routing.module';
 import { GradebookComponent } from './gradebook.component';
-import { StudentComponent } from './student/student.component';
 import { AssignmentComponent } from './assignment/assignment.component';
 import { AssignmentGradeComponent } from './assignment-grade/assignment-grade.component';
 import { AssignmentGradesComponent } from './assignment-grades/assignment-grades.component';
@@ -14,7 +13,6 @@ import { StudentsListComponent } from './student-list/student-list.component';
 @NgModule({
   declarations: [
     GradebookComponent,
-    StudentComponent,
     AssignmentComponent,
     AssignmentGradeComponent,
     AssignmentGradesComponent,
